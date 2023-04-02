2262d5a72eea89120f0bfd236fb5fabe0ad46fdc
Veselin Yordanov <vescko332@gmail.com>
2023-04-02 18:00:17 +0300

    Added star for favorite games

--- src/components/Games/Game/Game.js
+++ src/components/Games/Game/Game.js
@@ -1,12 +1,17 @@
 import './game.css';
 
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import * as gameService from '../../../services/gameService';
+import * as request from '../../../services/requester';
+import { AuthContext } from '../../../contexts/AuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export const Game = () => {
     const [game, setGame] = useState({});
+    const { user } = useContext(AuthContext);
 
     const { gameId } = useParams();
 
@@ -16,6 +21,18 @@ export const Game = () => {
         });
     }, [gameId]);
 
+    const [favorites, setFavorites] = useState(false);
+
+    const addToFavorites = (gameId) => {
+        const userId = user?.userId;
+        request
+            .post('/api/favorites', { game_id: gameId, user_id: userId })
+            .then((response) => {
+                setFavorites((prevState) => !prevState);
+            })
+            .catch((error) => console.error(error));
+    };
+
     return (
         <section className="oneGame-section">
             <img className="oneGame-background-img" src={game.background_image} alt="background-img" />
@@ -23,7 +40,18 @@ export const Game = () => {
                 <Link className="oneGame-return-link" to="#">
                     Back
                 </Link>
-                <h1 className="oneGame-title">{game.name}</h1>
+                <h1 className="oneGame-title">
+                    {game.name}
+                    {user.username && (
+                        <button className="button-favorites" onClick={() => addToFavorites(game.id)}>
+                            {favorites ? (
+                                <FontAwesomeIcon icon={faStar} size="2xl" style={{ color: '#ffec00' }} />
+                            ) : (
+                                <FontAwesomeIcon icon={faStar} size="2xl" style={{ color: '#645a00' }} />
+                            )}
+                        </button>
+                    )}
+                </h1>
                 <p className="omeGame bold_p">
                     Release Date:
                     <span className="omeGame normal_span"> {game.released}</span>
@@ -43,7 +71,7 @@ export const Game = () => {
                     <>
                         <h5 className="oneGame-description-title">Game Trailer</h5>
                         <div className="trailer-container">
-                            <video width="100%" height="600" controls src={game.trailer} autoplay>
+                            <video width="100%" height="600" controls src={game.trailer}>
                                 Your browser does not support the video tag.
                             </video>
                         </div>
